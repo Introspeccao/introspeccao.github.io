2a1c0b0641ab969ee4477248b14361788fa4d62c
João Gaião <dactyllo@gmail.com>
2024-09-03 19:00:29 +0100

    Adding Manifest and logic for PWA

--- service-worker.js
+++ service-worker.js
@@ -59,7 +59,7 @@ self.addEventListener("activate", (event) => {
     }
 });*/
 
-self.addEventListener("fetch", (event) => {
+/*self.addEventListener("fetch", (event) => {
     // We only want to call event.respondWith() if this is a navigation request
     // for an HTML page.
     if (event.request.mode === "navigate") {
@@ -95,4 +95,4 @@ self.addEventListener("fetch", (event) => {
     // chance to call event.respondWith(). If no fetch handlers call
     // event.respondWith(), the request will be handled by the browser as if there
     // were no service worker involvement.
-});
+});*/
